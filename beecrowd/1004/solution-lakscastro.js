9e74384d43e7e3ba678977902a7a20f89bb7ac1a
CarlosZoft <carlosk2gunn@hotmail.com>
2022-05-24 13:09:45 -0300

    ♻️ refactor: improving treatment for include more solutions

--- beecrowd/1004/solution-lakscastro.js
+++ beecrowd/1004/solution-lakscastro.js
@@ -1,5 +1,5 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
+const input = require("fs").readFileSync("/dev/stdin", "utf8");
+const lines = input.split("\n");
 
 const a = Number(lines.shift());
 const b = Number(lines.shift());
